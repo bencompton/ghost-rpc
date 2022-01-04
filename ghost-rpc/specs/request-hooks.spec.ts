cfbd51ef9daf214857c31cf801a8872605b38a17
Ben Compton <3343482+bencompton@users.noreply.github.com>
2022-01-04 07:58:30 -0700

    Add Jest test setup

--- ghost-rpc/specs/request-hooks.spec.ts
+++ ghost-rpc/specs/request-hooks.spec.ts
@@ -1,27 +1,27 @@
 describe('Request Hooks', () => {
   describe('When I add no Request Hooks and invoke a service', () => {
-    it('should directly execute the service');
+    it.todo('should directly execute the service');
   });
 
   describe('When I add one Request Hook and invoke a service', () => {
-    it('should run my Request hook before running the service');
+    it.todo('should run my Request hook before running the service');
 
     describe('When I call next() from my Request Hook', () => {
-      it('should invoke my service and return an object with a Service Execution Result property');
+      it.todo('should invoke my service and return an object with a Service Execution Result property');
     });
   });
 
   describe('When I add two request Hooks and invoke a service', () => {
     describe('When I call next() from the first Request Hook', () => {
-      it('should invoke the second Request Hook and return its Service Execution Result payload');
+      it.todo('should invoke the second Request Hook and return its Service Execution Result payload');
     });
 
     describe('When I call next() from the second Request Hook', () => {
-      it('should invoke my service and return an object with a Service Execution Result property');
+      it.todo('should invoke my service and return an object with a Service Execution Result property');
     });
   });
 
   describe('When I call next() more than once from a Request Hook', () => {
-    it('should throw an error');
+    it.todo('should throw an error');
   });
 });
